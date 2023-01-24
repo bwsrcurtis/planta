1656ebe5311eac4fe6fb2a849c02b5397ed228dd
bwsrcurtis <bwsrcurtis@gmail.com>
2023-01-24 15:26:49 -0500

    Added meta info

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,26 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {' '}
+        <meta
+          name="description"
+          content="Make Plant Care Easy"
+        />
+        <meta
+          http-equiv="Content-Type"
+          content="text/html;charset=UTF-8"
+        />
+        <meta
+          name="keywords"
+          content="plant planner care reminder"
+        />
+        <link
+          rel="shortcut icon"
+          href="favicon.ico"
+          type="image/x-icon"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import Head from 'next/head';
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  return (
+    <>
+      <Head>
+        <title>Planta App</title>
+      </Head>
+      <div>Welcome to Next.js!</div>
+    </>
+  );
 }
 
 export default HomePage;
