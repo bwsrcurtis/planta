783b44af614988a757ee65d0d675fea9de8ecd92
bwsrcurtis <bwsrcurtis@gmail.com>
2023-03-20 14:30:52 -0400

    Added signup page

--- src/pages/plants.tsx
+++ src/pages/plants.tsx
@@ -8,20 +8,24 @@ const Plants = () => {
 	// const [inputValue, setInputValue] = useState("");
 	const router = useRouter();
 
-	let auth = false;
-	if (auth) {
-		return (
-			<>
-				<Head>
-					<title>My Plants</title>
-				</Head>
-				<div>plants</div>
-			</>
-		);
-	}
-	else {
-		useEffect(() => { router.push('/login'); }, []);;
-	}
+	let auth = true;
+
+	useEffect(() => {
+		const checkAuth = async () => {
+			if (auth) {
+				return (
+					<>
+						<Head>
+							<title>My Plants</title>
+						</Head>
+						<h1>plants</h1>
+					</>
+				);
+			}
+			else { return router.push('/login'); }
+		};
+		checkAuth();
+	}, [auth, router]);;
 };
 
 export default Plants;
