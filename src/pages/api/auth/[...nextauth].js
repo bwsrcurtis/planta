203d2b07361ef3565e5f36583cd57ac8c3fde97a
bwsrcurtis <bwsrcurtis@gmail.com>
2023-04-03 18:35:16 -0400

    secret

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -11,7 +11,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-  secret: process.env.secret,
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async session({ session, token, user }) {
       session.user.id = user.id;
